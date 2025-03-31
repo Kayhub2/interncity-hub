f25d5911ea87a97af4ea74299ebffca3d5dcc3f4
Kayhub2 <feelingkay10@gmail.com>
2025-03-31 00:36:16 -0700

    Removed base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Replace 'your-repo-name' with your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/interncity-hub/', 
 });
+
